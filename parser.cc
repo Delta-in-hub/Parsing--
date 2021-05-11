b55df2ee6ef9e1cb429698376080fd97ad484830
Delta-in-hub <tombrown796@gmail.com>
2021-05-11 21:24:12 +0800

    getFirstSet done

---
+++ parser.cc
@@ -0,0 +1,10 @@
+#include "./parser.h"
+#include <bits/stdc++.h>
+using namespace std;
+#define debug(x) std::cout << #x << " is " << (x) << '\n'
+
+signed main(void)
+{
+    Parser::LL1 parse("grammar/1.txt");
+    return 0;
+}
---
+++ parser.h
@@ -0,0 +1,212 @@
+#ifndef PARSER__
+#define PARSER__
+#include <algorithm>
+#include <fstream>
+#include <stdexcept>
+#include <string>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+namespace Parser
+{
+class LL1
+{
+    const std::string ENDCHAR{"END"};
+    const std::string NULLCHAR{"NULL"};
+    struct production
+    {
+        bool nullable = false;
+        std::vector<std::vector<std::string>> symbol;
+    };
+
+    std::unordered_set<std::string> terminal;
+    std::unordered_map<std::string, production> nonterminal;
+
+    std::unordered_map<std::string, std::unordered_set<std::string>> firstSet;
+    std::unordered_map<std::string, std::unordered_set<std::string>> followSet;
+    //return the first nonterminal
+    std::string constructGrammar(const std::string& s)
+    {
+        using namespace std;
+        fstream fs(s);
+        if (not fs.is_open())
+            throw invalid_argument("File don't exist.");
+        string str;
+        string firstTerminal;
+        bool isFirst = true;
+        while (getline(fs, str))
+        {
+            if (str[0] == ';')
+                continue;
+            //E ::= T E' ;comments
+            string nonter;
+            // size_t pos = 0;
+            auto pos = str.find("::=");
+            if (pos == string::npos)
+            {
+                throw invalid_argument("production grammar error.\n");
+            }
+            for (size_t i = 0; i < pos; i++)
+            {
+                if (not isspace(str[i]))
+                    nonter.push_back(str[i]);
+            }
+            if (isFirst)
+            {
+                isFirst       = false;
+                firstTerminal = nonter;
+            }
+            pos += 3;
+            while (isspace(str[pos]))
+                pos++;
+            string ter;
+            vector<vector<string>> arr;
+            arr.push_back({});
+            size_t cnt = 0;
+            while (pos != str.size() + 1)
+            {
+                if (pos == str.size())
+                {
+                    if (not ter.empty())
+                    {
+                        if (ter == "<null>")
+                        {
+                            nonterminal[nonter].nullable = true;
+                            arr.pop_back();
+                            --cnt;
+                        }
+                        else
+                        {
+                            arr[cnt].push_back(move(ter));
+                        }
+                        ter.clear();
+                    }
+                    break;
+                }
+                if (str[pos] == ';')
+                {
+                    break;
+                }
+                if (str[pos] != ' ' and str[pos] != '|')
+                {
+                    ter.push_back(str[pos]);
+                }
+                else if (str[pos] == '|')
+                {
+                    arr.push_back({});
+                    ++cnt;
+                }
+                else if (not ter.empty())
+                {
+                    if (ter == "<null>")
+                    {
+                        nonterminal[nonter].nullable = true;
+                        arr.pop_back();
+                        --cnt;
+                    }
+                    else
+                    {
+                        arr[cnt].push_back(move(ter));
+                    }
+                    ter.clear();
+                }
+                ++pos;
+            }
+            nonterminal[nonter].symbol = move(arr);
+        }
+        return firstTerminal;
+    }
+    void findAllTerminal()
+    {
+        for (auto&& i : nonterminal)
+        {
+            for (auto&& j : i.second.symbol)
+            {
+                for (auto&& k : j)
+                {
+                    if (nonterminal.find(k) == nonterminal.end())
+                        terminal.emplace(k);
+                }
+            }
+        }
+    }
+    void getFirst()
+    {
+        using namespace std;
+
+        bool isChanged = false;
+        while (true)
+        {
+            isChanged = false;
+            for (auto&& i : nonterminal)
+            {
+                if (i.second.nullable)
+                {
+                    isChanged = firstSet[i.first].insert(NULLCHAR).second;
+                }
+                for (auto&& j : i.second.symbol)
+                {
+                    auto&& fron = j.front();
+                    if (terminal.find(fron) != terminal.end())
+                    {
+                        isChanged = firstSet[i.first].insert(fron).second;
+                    }
+                    else
+                    {
+                        bool allNull = true;
+                        for (auto&& k : j)
+                        {
+                            auto pos = nonterminal.find(k);
+                            if (pos != nonterminal.end())
+                            {
+                                for (auto&& m : firstSet[pos->first])
+                                {
+                                    if (m != ENDCHAR)
+                                    {
+                                        isChanged = firstSet[i.first].insert(m).second;
+                                    }
+                                }
+                                if (not pos->second.nullable)
+                                {
+                                    allNull = false;
+                                    break;
+                                }
+                            }
+                            else
+                            {
+                                allNull   = false;
+                                isChanged = firstSet[i.first].insert(k).second;
+                                break;
+                            }
+                        }
+                        if (allNull)
+                        {
+                            isChanged = firstSet[i.first].insert(NULLCHAR).second;
+                        }
+                    }
+                }
+            }
+            if (not isChanged)
+                break;
+        }
+    }
+    void getFollow()
+    {
+        ;
+    }
+
+  public:
+    LL1(const std::string& filePath)
+    {
+        auto&& res = constructGrammar(filePath);
+        nonterminal["_START_"].symbol.push_back({move(res), ENDCHAR});
+        findAllTerminal();
+        getFirst();
+        getFollow();
+    }
+};
+} // namespace Parser
+
+#endif
